ac5fd29f25cf9c81363da8fc4c477a69b7e42db1
Nils Wimmer <nils.wimmer@namics.com>
2019-05-21 12:15:04 +0200

    Create new flutter app with swift support.

--- lib/presenter/settings.dart
+++ lib/presenter/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:add_2_calendar/add_2_calendar.dart';
 
 class Settings extends StatelessWidget {
   @override
@@ -10,14 +11,14 @@ class Settings extends StatelessWidget {
             child: RaisedButton(
               child: Text('Export to Calendar'),
               onPressed: () {
-//                final Event event = Event(
-//                  title: 'Event title',
-//                  description: 'Event description',
-//                  location: 'Event location',
-//                  startDate: DateTime(2019, 5, 24, 12, 0, 0, 0, 0),
-//                  endDate: DateTime(2019, 5, 24, 13, 0, 0, 0, 0),
-//                );
-              //  Add2Calendar.addEvent2Cal(event);
+                final Event event = Event(
+                  title: 'Event title',
+                  description: 'Event description',
+                  location: 'Event location',
+                  startDate: DateTime(2019, 5, 24, 12, 0, 0, 0, 0),
+                  endDate: DateTime(2019, 5, 24, 13, 0, 0, 0, 0),
+                );
+                Add2Calendar.addEvent2Cal(event);
               },
             )));
   }
