06d9f04efd8c0fa859d7a24e7acf2091393d14d7
Nils Wimmer <nils.wimmer@namics.com>
2019-05-23 14:54:04 +0200

    Deleted Print statements

--- easy_study/lib/presenter/Settings.dart
+++ easy_study/lib/presenter/Settings.dart
@@ -4,24 +4,24 @@ import 'package:flutter/material.dart';
 import 'package:add_2_calendar/add_2_calendar.dart';
 
 class Settings extends StatelessWidget {
-
   List<Event> events;
   Event event;
 
   @override
   Widget build(BuildContext context) {
-    Future<List<Subject>> subjects =  DBHelper().getSubjects();
+    Future<List<Subject>> subjects = DBHelper().getSubjects();
     subjects.then((value) {
-      for (var subject in value)  {
-      print(subject.hoursWeek);
-      event = Event(
-        title: 'Event title',
-        description: 'Event description',
-        location: 'Event location',
-        startDate: DateTime(subject.dueDate.year, subject.dueDate.month, subject.dueDate.day, 1, 0, 0, 0, 0),
-        endDate: DateTime(subject.dueDate.year, subject.dueDate.month, subject.dueDate.day, 1 + subject.hoursWeek, 0, 0, 0, 0),
-      );
-    }
+      for (var subject in value) {
+        event = Event(
+          title: 'Event title',
+          description: 'Event description',
+          location: 'Event location',
+          startDate: DateTime(subject.dueDate.year, subject.dueDate.month,
+              subject.dueDate.day, 1, 0, 0, 0, 0),
+          endDate: DateTime(subject.dueDate.year, subject.dueDate.month,
+              subject.dueDate.day, 1 + subject.hoursWeek, 0, 0, 0, 0),
+        );
+      }
     });
     return Scaffold(
         body: Container(
