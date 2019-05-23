368fea184d7c18adfc2998b9883a8eca860ac4a3
Marcel Reineck <reineck@hm.edu>
2019-05-23 08:18:28 +0200

    Removed imports at test, Reformatted mymap.dart

--- lib/presenter/mymap.dart
+++ lib/presenter/mymap.dart
@@ -5,9 +5,9 @@ class MyMap extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-          alignment: Alignment.center,
-          child: Text("Map in construction!"),
-          // TODO: 03.05.2019 Import the map as a picture f.e.
-        ));
+      alignment: Alignment.center,
+      child: Text("Map in construction!"),
+      // TODO: 03.05.2019 Import the map as a picture f.e.
+    ));
   }
 }
