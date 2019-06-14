66346f86e6692ee967e0168849faf6868c90cbcf
Marcel Reineck <reineck@hm.edu>
2019-06-14 13:01:04 +0200

    added time tracking test

--- test/widget_tests/screens/screens_test.dart
+++ test/widget_tests/screens/screens_test.dart
@@ -1,14 +1,19 @@
+import 'package:easy_study/main.dart';
+import 'package:easy_study/model/exam_type.dart';
+import 'package:easy_study/model/priority.dart';
+import 'package:easy_study/model/subject.dart';
 import 'package:easy_study/presenter/hm_map.dart';
 import 'package:easy_study/presenter/settings.dart';
 import 'package:easy_study/presenter/subject_add.dart';
 import 'package:easy_study/presenter/subject_edit_or_delete.dart';
-import 'package:easy_study/testhelper/test_helper.dart';
+import 'package:easy_study/store/app_state.dart';
 import 'package:easy_study/view/home.dart';
 import 'package:easy_study/view/progress_summary.dart';
 import 'package:easy_study/view/subject_card.dart';
 import 'package:easy_study/view/subject_overview.dart';
-import 'package:easy_study/view/subject_progress_bar.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:redux/redux.dart';
 
 void main() {
   testWidgets('show subject overview', (tester) async {
@@ -17,34 +22,19 @@ void main() {
     await tester.pump();
   });
 
-  testWidgets('show subject card progress bar', (tester) async {
+  testWidgets('show subject progress bar', (tester) async {
     var createApp = TestHelper.createApp(
         SubjectCardProgressBar(TestHelper.getDummySubject()));
     await tester.pumpWidget(createApp);
     await tester.pump();
   });
 
-  testWidgets('show subject progress bar', (tester) async {
-    var createApp = TestHelper.createApp(SubjectProgressBar(
-      subject: TestHelper.getDummySubject(),
-    ));
-    await tester.pumpWidget(createApp);
-    await tester.pump();
-  });
-
   testWidgets('show progress summary', (tester) async {
     var createApp = TestHelper.createApp(ProgressSummary(
-        [TestHelper.getDummySubject2(), TestHelper.getDummySubject2()]));
+        [TestHelper.getDummySubject(), TestHelper.getDummySubject()]));
     await tester.pumpWidget(createApp);
     await tester.pump();
   });
-  testWidgets('show progress bar', (tester) async {
-    var createApp = TestHelper.createApp(
-        SubjectProgressBar(subject: TestHelper.getDummySubject2()));
-    await tester.pumpWidget(createApp);
-    await tester.pump();
-  });
-
   testWidgets('show subject card', (tester) async {
     var createApp = TestHelper.createApp(SubjectCard(
       subject: TestHelper.getDummySubject(),
@@ -74,6 +64,14 @@ void main() {
 
   testWidgets('show subject add', (tester) async {
     var createApp = TestHelper.createApp(SubjectAdd());
+    await tester.pumpWidget(createApp);
+    await tester.pump();
+  });
+
+  testWidgets('show subject edit or delete', (tester) async {
+    var createApp = TestHelper.createApp(SubjectEditOrDelete(
+      subject: TestHelper.getDummySubject(),
+    ));
     await tester.pumpWidget(createApp);
     await tester.pump();
   });
