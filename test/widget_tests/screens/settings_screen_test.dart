a584ffcf5adb3b2516386a6569bf87a6d1b947af
Nils Wimmer <nils.wimmer@namics.com>
2019-06-25 10:06:29 +0200

    Added some tests

--- test/widget_tests/screens/settings_screen_test.dart
+++ test/widget_tests/screens/settings_screen_test.dart
@@ -1,3 +1,7 @@
+import 'package:device_calendar/device_calendar.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+
 void main() {
   // TODO: 14.06.2019 not working because of channel implementation not found.
 //  test('settings screen addEventsToCalendar', () async {
@@ -8,4 +12,33 @@ void main() {
 //    list.add(TestHelper.getDummySubject());
 //    await createState.addEventsToCalendar(list);
 //  });
+  MethodChannel channel =
+      const MethodChannel('plugins.builttoroam.com/device_calendar');
+  DeviceCalendarPlugin deviceCalendarPlugin = new DeviceCalendarPlugin();
+
+  setUp(() {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      print("Calling channel method ${methodCall.method}");
+
+      return null;
+    });
+  });
+
+  test('HasPermissions_Returns_Successfully', () async {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      if (methodCall == 'hasPermissions') {
+        print("Has Permissions wurde aufgerufen");
+        return true;
+      } else if (methodCall == '') {
+        return true;
+      } else {
+        return "[{\"id\":\"1\",\"isReadOnly\":false,\"name\":\"fakeCalendarName\"}]";
+      }
+    });
+
+    final result = await Settings().retrieveCalendar();
+    expect(result.isSuccess, true);
+    expect(result.errorMessages, isEmpty);
+    expect(result.data, true);
+  });
 }
