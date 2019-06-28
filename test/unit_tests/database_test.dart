f23fc7f74d250b99322ea3adab9d769530b79319
Moritz Laur <laur@hm.edu>
2019-06-28 12:26:14 +0200

    added first mocked db test

--- test/unit_tests/database_test.dart
+++ test/unit_tests/database_test.dart
@@ -1,33 +1,35 @@
-//import 'package:easy_study/database/db_helper.dart';
-//import 'package:easy_study/model/exam_type.dart';
-//import 'package:easy_study/model/priority.dart';
-//import 'package:easy_study/model/subject.dart';
-//import 'package:flutter_test/flutter_test.dart';
-//
-////
+import 'package:easy_study/database/db_helper.dart';
+import 'package:easy_study/model/exam_type.dart';
+import 'package:easy_study/model/priority.dart';
+import 'package:easy_study/model/subject.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class MockClass extends Mock implements DBHelper {}
+
+DBHelper mockDBHelper = MockClass();
+
 void main() {
-//  Subject _getDummySubject() {
-//    return Subject.name(
-//        "Software Engineering II",
-//        ExamType.WRITTEN_EXAM,
-//        "T1.011",
-//        Priority.MINIMALISM,
-//        "A funny subject.",
-//        5,
-//        DateTime.parse('2019-06-10 16:27:46.371368'),
-//        DateTime.parse('2019-06-13 09:06:27.669877'));
-//  }
-//
-//  testWidgets('database: init database', (tester) async {
-//    var dbHelper = DBHelper();
-//    var database = await dbHelper.initDB();
-//  });
-//
-//  test('database: getSubjects', () async {
-//    var dbHelper = DBHelper();
-//    var list = await dbHelper.getSubjects();
-//  });
-//
+  Subject _getDummySubject() {
+    return Subject.name(
+        "Software Engineering II",
+        ExamType.WRITTEN_EXAM,
+        "T1.011",
+        Priority.MINIMALISM,
+        "A funny subject.",
+        5,
+        DateTime.parse('2019-06-10 16:27:46.371368'),
+        DateTime.parse('2019-06-13 09:06:27.669877'));
+  }
+
+  test('database: getSubjects', () async {
+    var dbHelper = mockDBHelper;
+    Future<List<Subject>> resultList = Future.value([_getDummySubject()]);
+    when(dbHelper.getSubjects()).thenAnswer((_)=> resultList);
+    var list = await dbHelper.getSubjects();
+    expect(list, await resultList);
+  });
+
 //  test('database: addNewSubject', () async {
 //    var dbHelper = DBHelper();
 //    var i = await dbHelper.addNewSubject(_getDummySubject());
