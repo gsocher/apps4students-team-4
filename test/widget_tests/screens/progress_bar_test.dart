0ad179ca34f860ac8ad63d04da1fba53bb6b0dde
Andreas Stiglmeier <stiglmeier.andreas@gmail.com>
2019-07-01 17:21:31 +0200

    add progressbar timetrack switch test

--- test/widget_tests/screens/progress_bar_test.dart
+++ test/widget_tests/screens/progress_bar_test.dart
@@ -1,10 +1,23 @@
 import 'package:easy_study/testhelper/test_helper.dart';
 import 'package:easy_study/view/subject_progress_bar.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 
 void main() {
   testWidgets('test subject progress bar', (tester) async {
-    var createApp = TestHelper.createApp(SubjectProgressBar(subject: TestHelper.getDummySubject()));
+    var createApp = TestHelper.createApp(
+        SubjectProgressBar(subject: TestHelper.getDummySubject()));
     await tester.pumpWidget(createApp);
+  });
+
+  testWidgets('test subject progress bar with switch to timetrack',
+      (tester) async {
+    var createApp = TestHelper.createApp(SubjectProgressBar(
+      subject: TestHelper.getDummySubject3(),
+      analytics: new FirebaseAnalytics(),
+    ));
+    await tester.pumpWidget(createApp);
+    await tester.tap(find.byType(SubjectProgressBar));
+    await tester.pump();
   });
 }
