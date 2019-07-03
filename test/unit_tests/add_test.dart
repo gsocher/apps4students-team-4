44c302149e2b3e34c1bd2ff42d67e24161e93dbc
UgoCRIS <33908191+UgoCRIS@users.noreply.github.com>
2019-07-03 10:50:49 +0200

    travis 2

--- test/unit_tests/add_test.dart
+++ test/unit_tests/add_test.dart
@@ -9,15 +9,6 @@ void main() {
     await tester.pumpWidget(createApp);
     await tester.pump();
     await tester.tap(find.byIcon(Icons.add));
-
-    //await tester.tap(find.byType(TextFormField));
-    //await tester.showKeyboard(find.by(TextFormField));
-    //await tester.enterText(find.byType(TextFormField).first,"title");
-    //await tester.enterText(find.bySemanticsLabel('ROOM'), '1002');
-    //await tester.enterText(find.bySemanticsLabel('DESCRIPTION'), 'blablabla');
-    //await tester.enterText(find.bySemanticsLabel('DUE_DATE'),'Sun');
-    //await tester.enterText(find.bySemanticsLabel('HOURS_PER_WEEK'),'12');
     await tester.tap(find.byIcon((Icons.save)));
-    //await tester.tap(find.byIcon((Icons.save)));
   });
 }
