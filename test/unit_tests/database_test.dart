4ae9c6deed4b33e8a800f1f1941f6d38f0f91bb9
Moritz Laur <laur@hm.edu>
2019-07-03 19:00:53 +0200

    added some tests

--- test/unit_tests/database_test.dart
+++ test/unit_tests/database_test.dart
@@ -1,7 +1,4 @@
 import 'package:easy_study/database/db_helper.dart';
-import 'package:easy_study/model/exam_type.dart';
-import 'package:easy_study/model/priority.dart';
-import 'package:easy_study/model/subject.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:sqflite/sqflite.dart';
@@ -9,28 +6,19 @@ import 'package:sqflite/sqlite_api.dart';
 
 class MockDatabase extends Mock implements Database {}
 class MockSqflite extends Mock implements Sqflite {}
+class MockedDbHelper extends Mock implements DBHelper{}
+
 
 Database mockDatabase = MockDatabase();
 Sqflite mockSqflite = MockSqflite();
 
 void main() {
-  Subject _getDummySubject() {
-    return Subject.name(
-        "Software Engineering II",
-        ExamType.WRITTEN_EXAM,
-        "T1.011",
-        Priority.MINIMALISM,
-        "A funny subject.",
-        5,
-        DateTime.parse('2019-06-10 16:27:46.371368'),
-        DateTime.parse('2019-06-13 09:06:27.669877'));
-  }
 
   test('database: createDB', () async {
     var databaseMocked = mockDatabase;
     DBHelper sut = DBHelper();
     sut.createDB(databaseMocked,1);
-    //when(sut.createDB(mockDatabase, 1)).thenAnswer((_)=> Future.value(databaseMocked));
+
 
     String sql = "CREATE TABLE Subject(id INTEGER PRIMARY KEY AUTOINCREMENT,"
         "  title TEXT, type TEXT, room TEXT, priority TEXT, description TEXT,"
@@ -43,19 +31,17 @@ void main() {
   });
 
   test('database: initDB', () async {
+    DBHelper sut = MockedDbHelper();
     var databaseMocked = mockDatabase;
+
+    when(sut.initDB()).thenAnswer((_)=> Future.value(databaseMocked));
+
+verifyNever(sut.initDB());
+  });
+
+  test('database: get database', () async {
     DBHelper sut = DBHelper();
-    sut.initDB();
-    when(sut.createDB(mockDatabase, 1)).thenAnswer((_)=> Future.value(databaseMocked));
-
-    String sql = "CREATE TABLE Subject(id INTEGER PRIMARY KEY AUTOINCREMENT,"
-        "  title TEXT, type TEXT, room TEXT, priority TEXT, description TEXT,"
-        " hoursWeek INTEGER, color_alpha INTEGER, color_red INTEGER,"
-        " color_green INTEGER, color_blue INTEGER, started_timetracking_at"
-        " TEXT, due_date TEXT, time_spent INTEGER, date_of_creation TEXT);";
-
-    verify();
-
+    sut.database;
   });
 
 
